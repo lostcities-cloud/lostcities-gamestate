76b336eb43c064bec79d02f897a2d81b64b5e8ee
Derek <contact@dereknelson.io>
2022-02-15 10:46:15 -0500

    Adding MatchEventService.kt

--- src/main/kotlin/io/dereknelson/lostcities/gamestate/matchevents/MatchEventService.kt
+++ src/main/kotlin/io/dereknelson/lostcities/gamestate/matchevents/MatchEventService.kt
@@ -1,5 +1,6 @@
 package io.dereknelson.lostcities.gamestate.matchevents
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.dereknelson.lostcities.models.matches.TurnChangeEvent
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
@@ -7,13 +8,16 @@ import org.springframework.stereotype.Component
 
 @Component
 class MatchEventService(
+    private val objectMapper: ObjectMapper,
     private var rabbitTemplate: RabbitTemplate,
 ) {
     companion object {
         val TURN_CHANGE_EVENT = "turn-change-event"
     }
 
-    public fun sendTurnChangeEvent(id: Long, login: String) {
-        rabbitTemplate.convertAndSend(TURN_CHANGE_EVENT, TurnChangeEvent(id, login))
+    fun sendTurnChangeEvent(id: Long, login: String) {
+        rabbitTemplate.convertAndSend(TURN_CHANGE_EVENT,
+            objectMapper.writeValueAsBytes(TurnChangeEvent(id, login))
+        )
     }
 }
