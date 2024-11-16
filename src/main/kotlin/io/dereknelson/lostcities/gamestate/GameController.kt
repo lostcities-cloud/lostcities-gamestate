73898c773634c562ee8abc5c556d6df5b0e74a1c
nelson54 <me@dereknelson.io>
2024-11-16 18:35:18 -0500

    Cleaning up gamestate

--- src/main/kotlin/io/dereknelson/lostcities/gamestate/GameController.kt
+++ src/main/kotlin/io/dereknelson/lostcities/gamestate/GameController.kt
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.slf4j.LoggerFactory
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.core.annotation.AuthenticationPrincipal
